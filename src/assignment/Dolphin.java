947cc3f02da33e697901ed161bb36d372354e9f1
Arun Pratap Singh <arun@singsys.com>
2019-11-01 18:53:21 +0530

    Solution for the third question for part B.

---
+++ src/assignment/Dolphin.java
@@ -0,0 +1,17 @@
+package assignment;
+
+public class Dolphin {
+
+	Dolphin() {
+		System.out.println("I am a good swimmer.");
+	}
+
+	/*
+	 * Yes we can model the Dolphin without inheriting from the Fish class.
+	 */
+
+	/*
+	 * The duplication of the code and introduction of the unneeded overhead can be
+	 * avoided by using a non parameterized constructor for this class.
+	 */
+}
